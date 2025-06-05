3434ed68aecade0aec996eb48d5e2edd81475430
khuefpoly <khuetdph42841@fpt.edu.vn>
2025-06-05 15:44:17 +0700

    sửa lại tên folder model thành models

--- admin/index.php
+++ admin/index.php
@@ -5,11 +5,11 @@ require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 // Require toàn bộ file Controllers
 require_once './controllers/AdminDanhMucController.php';
-// require_once './controllers/AdminSanPhamController.php';
+require_once './controllers/AdminSanPhamController.php';
 
 // Require toàn bộ file Models
-require_once './model/AdminDanhMuc.php';
-// require_once './controllers/AdminSanPham.php';
+require_once './models/AdminDanhMuc.php';
+require_once './models/AdminSanPham.php';
 // Route
 $act = $_GET['act'] ?? '/';
  // Debug để xem giá trị của act và method
@@ -22,4 +22,12 @@ match ($act) {
    'form-sua-danh-muc' => (new AdminDanhMucController())->formEditDanhMuc(),
    'sua-danh-muc' => (new AdminDanhMucController())->postEditDanhMuc(),
    'xoa-danh-muc' => (new AdminDanhMucController())->deleteDanhMuc(),
+
+   // router sản phẩm
+   'san-pham' => (new AdminSanPhamController())->danhSachSanPham(),
+   'form-them-san-pham' => (new AdminSanPhamController())->formAddSanPham(),
+   'them-san-pham' => (new AdminSanPhamController())->postAddSanPham(),
+   'form-sua-san-pham' => (new AdminSanPhamController())->formEditSanPham(),
+   'sua-san-pham' => (new AdminSanPhamController())->postEditSanPham(),
+   'xoa-san-pham' => (new AdminSanPhamController())->deleteSanPham(),
 };
